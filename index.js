383aecc03c367abd4bbe1332d5314f3090f6c6dc
tommijuslin <tommi.juslin@gmail.com>
2022-03-20 20:15:09 +0200

    3.5: puhelinluettelon backend step5

--- index.js
+++ index.js
@@ -48,6 +48,26 @@ app.get('/info', (request, response) => {
   )
 })
 
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 10000)
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter(person => person.id !== id)
