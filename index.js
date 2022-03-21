e1f09139c0f4421dd4bd2bde4edd42fe49b5fe7b
tommijuslin <tommi.juslin@gmail.com>
2022-03-22 00:58:09 +0200

    3.20*: puhelinluettelo ja tietokanta, step8

--- index.js
+++ index.js
@@ -24,29 +24,6 @@ morgan.token('body', (request) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234235"
-  },
-  {
-    id: 4,
-    name: "Mary Poppendick",
-    number: "39-23-6423122"
-  }
-]
-
 app.get('/api/persons', (request, response) => {
   Person.find({})
     .then(people => {
@@ -75,23 +52,19 @@ app.get('/info', (request, response) => {
   })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
-  
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: 'name or number missing'
-    })
-  }
 
   const person = new Person({
     name: body.name,
     number: body.number,
   })
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -128,6 +101,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
--- models/person.js
+++ models/person.js
@@ -12,8 +12,21 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: [true, 'Name is required']
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: (v) => {
+        return v.length >= 8 && /\d{2,3}-\d{6,}/.test(v)
+      },
+      message: props => `${ props.value } is not a valid phone number`
+    },
+    required: [true, 'Number is required']
+  },
 })
 
 personSchema.set('toJSON', {
